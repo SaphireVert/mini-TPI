0761c16f44d8351b3608b131420dc536e1cfd953
SaphireVert <jerome.cosandey@gmail.com>
2022-06-27 17:26:34 +0200

    add research on title

--- kata-manga-app/src/pages/Mangas/Mangas.jsx
+++ kata-manga-app/src/pages/Mangas/Mangas.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import styled from "styled-components";
 // import Spinner from "./animations/Spinner"
+import { withRouter } from 'react-router-dom'
 
 
 
@@ -20,11 +21,20 @@ export default class Mangas extends React.Component {
     columns: null,
     rows: null,
     loading: true,
-    data: null
+    data: null,
+    params: undefined
   }
 
   async setRows () {
-    this.setState({ rows: this.state.data });
+    let finaldata = this.state.data.filter((element) => {
+            let filter = this.state.params
+            if (!filter) return true;
+            let title = element.title.toLowerCase();
+            // console.log(element.title);
+            return title.startsWith(filter.toLowerCase());
+          })
+          console.log(this.state.params);
+          this.setState({ rows: finaldata });
   }
   async setColumns () {
     let dataStateArray = Object.keys(this.state.data[0])
@@ -39,6 +49,17 @@ export default class Mangas extends React.Component {
     await this.setRows()
   }
   async componentDidMount(){
+      const test = new URLSearchParams(window.location.search)
+      test.set( "filter", "filter" )
+      const params = new URLSearchParams("toto");
+      // const q = parseInt(params.get("toto")); // is the number 123
+
+      const queryString = window.location.search;
+      console.log(queryString);
+      const urlParams = new URLSearchParams(queryString).get("toto");
+      // const toto = urlParams
+      console.log(urlParams);
+
       let arresults = []
       for (let i = 1; i <= 100; i++) {
         const res = await (await fetch(`http://localhost:3004/manga/${i}`)).json()
@@ -55,15 +76,37 @@ export default class Mangas extends React.Component {
     return (
       <Container>
             {this.state.loading ? (
-              <div>Loading</div>
+              <div>Loading {new URLSearchParams(window.location.search).get("toto")}</div>
             ) : (
+              <div style={{ height: 300, width: '100%' }}>
+                <input
+                  value={this.state.params}
+                  onChange={(event) => {
+                    let filter = event.target.value;
+                    if (filter) {
+                      console.log(filter);
+                      this.setState({params: filter});
+                      this.setRows()
+                    } else {
+                      this.setState({params: ""});
+                      this.setRows()
+                    }
+                  }}
+                />
                 <DataGrid
-                  rows={this.state.rows}
+                  rows={this.state.data.filter((element) => {
+                    let filter = this.state.params
+                    if (!filter) return true;
+                    let title = element.title.toLowerCase();
+                    // console.log(element.title);
+                    return title.includes(filter.toLowerCase());
+                  })}
                   columns={this.state.columns}
                   pageSize={10}
                   rowsPerPageOptions={[10, 15, 20, 25]}
                   // checkboxSelection
                 />
+              </div>
             )}
       </Container>
     );
